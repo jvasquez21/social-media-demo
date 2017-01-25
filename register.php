e3ad2fadaa249446e3ad21c47c9586bba87a052a
jvasquez21 <juliavasquez69@gmail.com>
2017-01-24 19:22:10 -0600

    register.php
    Created two fields first name and last name.

---
+++ register.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Welcome to Systems!</title>
+</head>
+<body>
+
+    <form>
+        <input type="text" name="reg_fname" placeholder="First Name" required>
+        <input type="text" name="reg_lname" placeholder="Last Name" required>
+
+    </form>
+
+
+
+</body>
+</html>
