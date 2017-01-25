e86d7bc245ddf772cebc1886a11681fe97a89c61
jvasquez21 <juliavasquez69@gmail.com>
2017-01-24 20:54:36 -0600

    Update register.php
    Added the rest of the registration form values

--- register.php
+++ register.php
@@ -15,13 +15,31 @@
   $error_array = ""; //holds error messages
  if(isset($POST['register_button'])){
      //registration form values
-     $fname = strip_tags($_POST['reg_fname']); //strip_tags security measures such as <a> name <a>
-     $fname = str_replace(' ', '', $fname); //remove spaces
-     $fname = ucfirst(strtolower($fname)); //convert letters to lowers and keeps first letter as upper case     
-     //registration form values
-     $lname = strip_tags($_POST['reg_lname']); //strip_tags security measures such as <a> name <a>
-     $lname = str_replace(' ', '', $lname); //remove spaces
-     $lname = ucfirst(strtolower($lname)); //convert letters to lowers and keeps first letter as upper case
+     //first name
+      $fname = strip_tags($_POST['reg_fname']); //strip_tags security measures such as <a> name <a>
+      $fname = str_replace(' ', '', $fname); //remove spaces
+      $fname = ucfirst(strtolower($fname)); //convert letters to lowers and keeps first letter as upper case
+      
+      //last name
+      $lname = strip_tags($_POST['reg_lname']); //strip_tags security measures such as <a> name <a>
+      $lname = str_replace(' ', '', $lname); //remove spaces
+      $lname = ucfirst(strtolower($lname)); //convert letters to lowers and keeps first letter as upper case
+     
+      //email
+      $em = strip_tags($_POST['reg_email']); //strip_tags security measures such as <a> name <a>
+      $em = str_replace(' ', '', $em); //remove spaces
+      $em = ucfirst(strtolower($em)); //convert letters to lowers and keeps first letter as upper case
+ 
+       //email2
+      $em2 = strip_tags($_POST['reg_email2']); //strip_tags security measures such as <a> name <a>
+      $em2 = str_replace(' ', '', $em2); //remove spaces
+      $em2 = ucfirst(strtolower($em2)); //convert letters to lowers and keeps first letter as upper case
+  
+      //password
+     $password = strip_tags($_POST['reg_password']); //removes html tags
+     $password2 = strip_tags($_POST['reg_password2']); //removes html tags
+ 
+     $date = date("Y-m-d"); //gets the current date 
  
   }
 ?>
